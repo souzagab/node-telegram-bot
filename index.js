1c6a7ce5c8f05ca11be56d6b9fb69857a4d05e50
Gabriel <desouza.gabriel@icloud.com>
2019-12-15 14:39:15 -0300

    feat(index.js): create hook for heroku
    Create webhook for heroku deployment

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const ax = require('axios')
 const TelegramBot = require('node-telegram-bot-api')
+const parser = require('./src/parser.js')
+const express = require('express')
+const bodyParser = require('body-parser');
 
 require('dotenv').config()
 
@@ -12,8 +15,6 @@ if (process.env.NODE_ENV === 'production') {
 } else {
   bot = new TelegramBot(token, { polling: true })
 }
-
-const parser = require('./src/parser.js')
 
 bot.onText(/\/word (.+)/, (msg, match) => {
   const chatId = msg.chat.id
@@ -36,4 +37,13 @@ bot.onText(/\/word (.+)/, (msg, match) => {
       const errorText = error.response.status === 404 ? `No definition found for: <b>${word}</b>` : `<b>Error occured, please try again later</b>`
       bot.sendMessage(chatId, errorText, { parse_mode: 'HTML' })
     })
+})
+
+const app = express()
+
+app.use(bodyParser.json())
+app.listen(process.env.PORT)
+app.post('/' + bot.token, (req, res) => {
+  bot.processUpdate(req.body)
+  res.sendStatus(200)
 })
